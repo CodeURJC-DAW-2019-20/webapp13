7670d622d23e8e4a863adbd2cb8a6bfea2666ebb
Rod <roddesiqueira@gmail.com>
2020-02-03 21:39:49 +0100

    Pestañas de evento actualizadas.

--- assets/accion.js
+++ assets/accion.js
@@ -7,9 +7,16 @@ function openEvent(){
 function mostrarArtistas() {
     document.getElementById('eventos').style.display="none";
     document.getElementById('artistas').style.display="block";
+    document.getElementById('noticias').style.display="none";
 
 }
 function mostrarFestivales() {
     document.getElementById('artistas').style.display="none";
     document.getElementById('eventos').style.display="block";
+    document.getElementById('noticias').style.display="none";
+}
+function mostrarNoticias() {
+    document.getElementById('artistas').style.display="none";
+    document.getElementById('eventos').style.display="none";
+    document.getElementById('noticias').style.display="block";
 }
